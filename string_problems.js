ea2dfce28e87868267249375e93795924dd89c4a
manish26eleven <118613667+manish26eleven@users.noreply.github.com>
2023-03-27 18:53:10 +0530

    Add files via upload

---
+++ string_problems.js
@@ -0,0 +1,11 @@
+//1.what will be output---->>
+console.log("har\"".length)
+//convert given string to lower case
+let name ="MANISH"
+console.log(name.toLowerCase())
+// extract the amt in this
+name = "i have 1000 rs"
+console.log(name.slice(7,11))
+//can we change a character of a givem string 
+no we can,t do that ,it is immutable
+we can make new string 
---
+++ strings.js
@@ -0,0 +1,22 @@
+let fruit='banana'
+console.log(fruit)
+// escape sequence character
+fruit='bana\'na'
+console.log(fruit)
+//string methods
+let name = 'manish'
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,4))
+console.log(name.slice(2))
+name="manish bhai"
+console.log(name.replace("bhai","bhau"))
+name ="manish"
+name1 ="kumar"
+console.log(name.concat(name1,"singh","rajpoot"))
+name="   manish   "
+console.log(name.trim())
+name="manish"
+console.log(name[0])
+let fullname = name + "kumar"
+console.log(fullname)
